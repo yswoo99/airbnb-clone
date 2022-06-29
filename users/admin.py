c616757e715c588ecd49f2777b9419f43619c01a
yswoo99 <yswoo99@gmail.com>
2022-06-29 18:41:30 +0900

    Finished by chapter 3.6

---
+++ users/admin.py
@@ -0,0 +1,28 @@
+from operator import mod
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
+
+# Register your models here.
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """Custom User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthday",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
---
+++ users/models.py
@@ -0,0 +1,37 @@
+from pickle import FALSE
+from random import choices
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    """Custom User Model"""
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_EN = "en"
+    LANGUAGE_KO = "ko"
+
+    LANGUAGE_CHOICES = ((LANGUAGE_EN, "English"), (LANGUAGE_KO, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    bio = models.TextField(default="", blank=True)
+    birthday = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, blank=True, max_length=2)
+    currency = models.CharField(choices=CURRENCY_CHOICES, blank=True, max_length=3)
+    superhost = models.BooleanField(default=False)
