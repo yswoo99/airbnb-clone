6f9aa16da22a82d81c28b0a0f9729fe28d8bd571
yswoo99 <yswoo99@gmail.com>
2022-07-01 17:20:32 +0900

    Finished by chapter 7.3

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,3 @@
-from statistics import mode
 from django.contrib import admin
 from . import models
 
@@ -7,6 +6,14 @@ from . import models
 class ItemAdmin(admin.ModelAdmin):
 
     """Item Admin Definition"""
+
+    list_display = (
+        "name",
+        "used_by",
+    )
+
+    def used_by(self, obj):
+        return obj.rooms.count()
 
     pass
 
@@ -24,4 +31,71 @@ class RoomAdmin(admin.ModelAdmin):
 
     """Room Admin Definition"""
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        (
+            "Times",
+            {"fields": ("check_in", "check_out", "instant_book")},
+        ),
+        (
+            "Spaces",
+            {"fields": ("guests", "beds", "bedroom", "baths")},
+        ),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedroom",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+        "count_photos",
+    )
+
+    def count_amenities(self, obj):
+        print(obj.amenities.all())
+        return obj.amenities.count()
+
+    count_amenities.short_description = "hello sexy!"
+
+    def count_photos(self, obj):
+        return obj.photos.count()
+
+    ordering = ("price", "bedroom")
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ("^city", "=host__username")
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,4 @@
+from mimetypes import guess_extension
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
@@ -55,7 +56,9 @@ class Photo(core_models.TimeStampedModel):
 
     caption = models.CharField(max_length=80)
     file = models.ImageField()
-    room = models.ForeignKey("Room", on_delete=models.CASCADE, null=True)
+    room = models.ForeignKey(
+        "Room", related_name="photos", on_delete=models.CASCADE, null=True
+    )
 
     def __str__(self):
         return self.caption
@@ -67,20 +70,26 @@ class Room(core_models.TimeStampedModel):
 
     name = models.CharField(max_length=140)
     description = models.TextField()
-    contry = CountryField()
+    country = CountryField()
     city = models.CharField(max_length=80)
     price = models.IntegerField()
     address = models.CharField(max_length=140)
+    guests = models.IntegerField(null=True)
     beds = models.IntegerField()
+    bedroom = models.IntegerField(null=True)
     baths = models.IntegerField()
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey("users.User", on_delete=models.CASCADE, null=True)
-    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField("Amenity", blank=True)
-    facilities = models.ManyToManyField("Facility", blank=True)
-    house_rules = models.ManyToManyField("HouseRule", blank=True)
+    host = models.ForeignKey(
+        "users.User", related_name="rooms", on_delete=models.CASCADE, null=True
+    )
+    room_type = models.ForeignKey(
+        "RoomType", related_name="rooms", on_delete=models.SET_NULL, null=True
+    )
+    amenities = models.ManyToManyField("Amenity", related_name="rooms", blank=True)
+    facilities = models.ManyToManyField("Facility", related_name="rooms", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", related_name="rooms", blank=True)
 
     def __str__(self):
         return self.name
