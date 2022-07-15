725334f508dd008ea47f2b1cf2bc9efcd03cf4b0
yswoo99 <yswoo99@gmail.com>
2022-07-15 18:30:08 +0900

    Finished by chapter 15.1 - generic form

--- users/views.py
+++ users/views.py
@@ -1,15 +1,55 @@
 from tkinter import Widget
 from django.forms import PasswordInput
 from django.views import View
+from django.views.generic import FormView
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
-# Create your views here.
+class LoginView(FormView):
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "email": "yswoo99@gmail.com"
+    }
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+
+        return super().form_valid(form)
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignupForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        'first_name': "Seungwoo",
+        "last_name": "Yang",
+        'email': "yswoo99@gmail.com"
+    }
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+'''
 class LoginView(View):
     def get(self, request):
-        form = forms.LoginForm(initial={"email": "seungwoo@google.com"})
+        form = forms.LoginForm(initial={"email": "yswoo99@gmail.com"})
         return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
@@ -28,3 +68,4 @@ class LoginView(View):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+'''
