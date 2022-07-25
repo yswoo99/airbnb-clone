eb5bd476611cecded0b134fa7a7d742534c038b4
yswoo99 <yswoo99@gmail.com>
2022-07-25 19:13:31 +0900

    Finished by 21.7 - login toast, update profile

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.shortcuts import reverse
 from django.db import models
 from django.core.mail import send_mail
 from django.utils.html import strip_tags
@@ -31,37 +32,53 @@ class User(AbstractUser):
 
     CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
 
-    LOGIN_EMAIL ="email"
+    LOGIN_EMAIL = "email"
     LOGIN_GITHUB = "github"
     LOGIN_KAKAO = "kakao"
 
-    LOGIN_CHOICES = ((LOGIN_EMAIL, "Email"), (LOGIN_GITHUB, "Github"), (LOGIN_KAKAO, "Kakao"))
+    LOGIN_CHOICES = (
+        (LOGIN_EMAIL, "Email"),
+        (LOGIN_GITHUB, "Github"),
+        (LOGIN_KAKAO, "Kakao"),
+    )
 
     avatar = models.ImageField(upload_to="avatars", blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(default="", blank=True)
     birthday = models.DateField(blank=True, null=True)
-    language = models.CharField(choices=LANGUAGE_CHOICES, blank=True, max_length=2, default=LANGUAGE_KO)
-    currency = models.CharField(choices=CURRENCY_CHOICES, blank=True, max_length=3, default=CURRENCY_KRW)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, blank=True, max_length=2, default=LANGUAGE_KO
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, blank=True, max_length=3, default=CURRENCY_KRW
+    )
     superhost = models.BooleanField(default=False)
     email_verified = models.BooleanField(default=False)
     email_secret = models.CharField(max_length=20, default="", blank=True)
-    login_method = models.CharField(max_length=30, choices=LOGIN_CHOICES, default=LOGIN_EMAIL)
+    login_method = models.CharField(
+        max_length=30, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
+    )
+
+    # For "VIEW ON SITE" in admin panel
+    def get_absolute_url(self):
+        return reverse("users:profile", kwargs={"pk": self.pk})
 
     def verify_email(self):
         print(self.email)
         if self.email_verified is False:
             secret = uuid.uuid4().hex[:20]
             self.email_secret = secret
-            html_message = render_to_string("emails/verify_email.html", {"secret": secret})
+            html_message = render_to_string(
+                "emails/verify_email.html", {"secret": secret}
+            )
             send_mail(
                 "Verify Yangbnb Account",
                 strip_tags(html_message),
-                settings.EMAIL_FROM, [self.email],
+                settings.EMAIL_FROM,
+                [self.email],
                 fail_silently=False,
-                html_message=html_message
+                html_message=html_message,
             )
             self.save()
 
         return
-
--- users/views.py
+++ users/views.py
@@ -3,10 +3,12 @@ import profile
 from unittest import result
 from django.forms import PasswordInput
 from django.views import View
-from django.views.generic import FormView
+from django.contrib.auth.views import PasswordChangeView
+from django.views.generic import FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.core.files.base import ContentFile
 from . import forms, models
 
@@ -15,11 +17,11 @@ class LoginView(FormView):
     template_name = "users/login.html"
     form_class = forms.LoginForm
     success_url = reverse_lazy("core:home")
-    '''
+    """
     initial = {
         "email": "yswoo99@gmail.com"
     }
-    '''
+    """
 
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
@@ -33,6 +35,7 @@ class LoginView(FormView):
 
 def log_out(request):
     logout(request)
+    messages.info(request, "See you later")
     return redirect(reverse("core:home"))
 
 
@@ -40,13 +43,13 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SignupForm
     success_url = reverse_lazy("core:home")
-    '''
+    """
     initial = {
         'first_name': "Seungwoo",
         "last_name": "Yang",
         'email': "yswoo99@gmail.com"
     }
-    '''
+    """
 
     def form_valid(self, form):
         form.save()
@@ -74,7 +77,9 @@ def complete_verification(request, key):
 def github_login(request):
     client_id = os.environ.get("GITHUB_ID")
     redirect_uri = "http://127.0.0.1:8000/users/login/github/callback"
-    return redirect(f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=read:user")
+    return redirect(
+        f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=read:user"
+    )
 
 
 class GithubException(Exception):
@@ -89,15 +94,21 @@ def github_callback(request):
         if code is not None:
             token = requests.post(
                 f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
-                headers={"Accept": "application/json"}
+                headers={"Accept": "application/json"},
             )
             token_json = token.json()
             error = token_json.get("error", None)
             if error is not None:
-                raise GithubException()
+                raise GithubException("Can't get access token")
             else:
                 access_token = token_json.get("access_token")
-                profile_request = requests.get(f"https://api.github.com/user", headers={"Authorization": f"token {access_token}", "Accept": "application/json"})
+                profile_request = requests.get(
+                    f"https://api.github.com/user",
+                    headers={
+                        "Authorization": f"token {access_token}",
+                        "Accept": "application/json",
+                    },
+                )
                 profile_json = profile_request.json()
                 print(profile_json)
                 username = profile_json.get("login", None)
@@ -107,33 +118,39 @@ def github_callback(request):
                     try:
                         user = models.User.objects.get(email=email)
                         if user.login_method != models.User.LOGIN_GITHUB:
-                            raise GithubException()
+                            raise GithubException(
+                                f"Please log in with: {user.login_method}"
+                            )
                     except models.User.DoesNotExist:
                         user = models.User.objects.create(
                             username=email,
                             first_name=name,
                             email=email,
                             login_method=models.User.LOGIN_GITHUB,
-                            email_verified=True
+                            email_verified=True,
                         )
                         user.set_unusable_password()
                         user.save()
 
                     login(request, user)
+                    messages.success(request, f"Welcome back {user.first_name}")
                     return redirect(reverse("core:home"))
 
                 else:
-                    raise GithubException()
+                    raise GithubException("Can't get you profile")
         else:
             raise GithubException()
-    except GithubException:
+    except GithubException as e:
+        messages.error(request, e)
         return redirect(reverse("users:login"))
 
 
 def kakao_login(request):
     client_id = os.environ.get("KAKAO_API_KEY")
     redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
-    return redirect(f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code")
+    return redirect(
+        f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code"
+    )
 
 
 class KakaoExeption(Exception):
@@ -146,11 +163,13 @@ def kakao_callback(request):
         print(f"code:{code}")
         client_id = os.environ.get("KAKAO_API_KEY")
         redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
-        token_request = requests.post(f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={redirect_uri}&code={code}")
+        token_request = requests.post(
+            f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={redirect_uri}&code={code}"
+        )
         token_json = token_request.json()
         error = token_json.get("error", None)
         if error is not None:
-            raise KakaoExeption()
+            raise KakaoExeption("Can't get authorization code.")
         access_token = token_json.get("access_token")
         profile_request = requests.get(
             "https://kapi.kakao.com/v2/user/me",
@@ -160,21 +179,21 @@ def kakao_callback(request):
         kakao_account = profile_json.get("kakao_account")
         email = kakao_account.get("email")
         if email is None:
-            raise KakaoExeption()
+            raise KakaoExeption("Please also give me your email")
         properties = profile_json.get("properties")
         nickname = properties.get("nickname")
         profile_image = properties.get("profile_image")
         try:
             user = models.User.objects.get(email=email)
             if user.login_method != models.User.LOGIN_KAKAO:
-                raise KakaoExeption()
+                raise KakaoExeption(f"Please log in with: {user.login_method}")
         except models.User.DoesNotExist:
             user = models.User.objects.create(
                 username=email,
                 first_name=nickname,
                 email=email,
                 login_method=models.User.LOGIN_KAKAO,
-                email_verified=True
+                email_verified=True,
             )
             user.set_unusable_password()
             user.save()
@@ -183,13 +202,42 @@ def kakao_callback(request):
                 photo_request = requests.get(profile_image)
                 user.avatar.save(f"{id}-avatar.jpg", ContentFile(photo_request.content))
         login(request, user)
+        messages.success(request, f"Welcome back {user.first_name}")
         return redirect(reverse("core:home"))
-    except KakaoExeption:
+    except KakaoExeption as e:
+        messages.error(request, e)
         return redirect(reverse("users:login"))
 
 
+class UserProfileView(DetailView):
 
-'''
+    model = models.User
+    context_object_name = "user_obj"
+
+
+class UpdateProfileView(UpdateView):
+
+    model = models.User
+    template_name = "users/update-profile.html"
+    fields = (
+        "first_name",
+        "last_name",
+        "gender",
+        "bio",
+        "language",
+        "currency",
+    )
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class UpdatePasswordView(PasswordChangeView):
+
+    template_name = "users/update-password.html"
+
+
+"""
 class LoginView(View):
     def get(self, request):
         form = forms.LoginForm(initial={"email": "yswoo99@gmail.com"})
@@ -211,4 +259,4 @@ class LoginView(View):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
-'''
+"""
