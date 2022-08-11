62be84da3e387e8b8d8a15dec61093b5eefeb24b
yswoo99 <yswoo99@gmail.com>
2022-08-11 18:53:23 +0900

    Add allowed host

--- config/settings.py
+++ config/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "julrq+8fl0ar84pem1904$lz6*j*nifyv-y*4f*@eax5o+6ob7"
+SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = bool(os.environ.get("DEBUG"))
 DEBUG = True
 
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = [".elasticbeanstalk.com"]
 
 
 # Application definition
