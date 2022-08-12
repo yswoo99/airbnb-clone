defebfd370db860fb3571ccc1967eca254d88968
yswoo99 <yswoo99@gmail.com>
2022-08-12 15:42:34 +0900

    Create superuser in AWS

--- users/models.py
+++ users/models.py
@@ -69,7 +69,7 @@ class User(AbstractUser):
     login_method = models.CharField(
         max_length=30, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
     )
-    objects = core_managers.CustomModelManager()
+    objects = core_managers.CustomUserManager()
 
     # For "VIEW ON SITE" in admin panel
     def get_absolute_url(self):
